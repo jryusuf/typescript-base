4dc1343c9db3cd51201c13bc79151062900d2322
Yusuf Yuksel <yusufyuksel@aol.com>
2024-11-05 10:50:18 +0000

    shadcn button demo

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-
+import { Button } from "@components/ui/button";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -94,7 +94,11 @@ export default function Home() {
             height={16}
           />
           Go to nextjs.org →
+          Hellow Word
+
+
         </a>
+        <Button>Hellow Word</Button>
       </footer>
     </div>
   );
