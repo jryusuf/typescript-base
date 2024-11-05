1627aca6cfe967fb9ee31508eb3626802da4b2e7
Yusuf Yuksel <yusufyuksel@aol.com>
2024-11-05 11:07:44 +0000

    this must fail

--- my-app/__tests__/utils.test.ts
+++ my-app/__tests__/utils.test.ts
@@ -7,3 +7,7 @@ test('adds 1 + 2 to equal 3', () => {
 test("this my fail", () => {
     expect(add(1, 2)).toBe(4);
 });
+
+test("this one also must failed", () => {
+    expect(add(1, 2)).toBe(5);
+});
